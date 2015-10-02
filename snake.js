190cd570ba6b68803eca48b94990dfef1b288b91
Alex Fowler <alexj_fowler@yahoo.com>
2015-10-02 15:09:31 -0400

    added grid lines to the snakes game

--- snake.js
+++ snake.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/snake.html');
 });
 
+app.get('/canvasFunctions.js', function(req, res) {
+  res.require(__dirname + '/canvasFunctions.js');
+});
+
 http.listen(3000, function() {
   console.log('listening on *:3000');
 });
